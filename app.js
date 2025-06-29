865d093779096ff962326562d33cc5dde7d3d3b9
Luffycoder0 <luffycoding0@gmail.com>
2025-06-29 08:11:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,32 +5,44 @@ const rateLimit = require('express-rate-limit');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const userRouter = require('./routers/userRouter');
 
 const app = express();
-// Middlewares
-// console.log(process.env.NODE_ENV);
 
+// Set security HTTP headers
 app.use(helmet());
 
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
-  message: 'Too many requests from this IP, plz try again in an hour!',
+  message: 'Too many requests from this IP, please try again in an hour!',
 });
-
 app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
 
-// Data sanitization against NoSQL injection
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    credentials: true,
+  }),
+);
+
+// Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
+
 // Data sanitization against XSS
 app.use(xss());
 
@@ -48,21 +60,24 @@ app.use(
   }),
 );
 
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-  next();
-});
+// Add request time
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
-// Routers
+// Routes
 app.use('/api/v1/users', userRouter);
 
+// Unhandled routes
 app.all('*', (req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on this server!`), 404);
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
+
+// Global error handling middleware
 app.use(globalErrorHandler);
+
 module.exports = app;
